466d9e29da993f9d0cdecb2da8b94f05a5c24f7e
Piyush1532 <piyushadav503@gmail.com>
2025-05-05 15:39:16 +0530

    fixed typo error

--- client/src/Components/Hero.jsx
+++ client/src/Components/Hero.jsx
@@ -6,7 +6,7 @@ const Hero = () => {
     <div className='flex flex-col items-start justify-center px-6 md:px-16 lg:px-24 xl:px-32 text-white bg-[url("/src/assets/heroImage.avif")] bg-no-repeat bg-cover bg-center h-screen'>
      <p className='bg-[#49B9FF]/50 px-3 py-1 rounded-full mt-20'>The Ultimate Hotel Experience</p> 
      <h1 className='font-playfair text-2xl md:text-5xl ,d:text-[56px] md:leading-[56px] font-bold md:font-extrabold max-w-xl mt-4'>Discover Your Perfect Gateway Destination </h1>
-     <p max-w-130 mt2 text-sm md:text-base>Unparalleled luxury and comfort await at the world's most exclusive hotels and resorts.Start your journey today. </p>
+     <p className='max-w-130 mt-2 text-sm md:text-base'>Unparalleled luxury and comfort await at the world's most exclusive hotels and resorts.Start your journey today. </p>
 
 
 {/* Form design */}
