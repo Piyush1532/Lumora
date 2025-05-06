25d86dcdb50979af2d57d3123ec252287a53017a
Piyush1532 <piyushadav503@gmail.com>
2025-05-06 14:45:36 +0530

    added featured destination component

--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Hero from '../Components/hero'
+import FeaturedDestination from '../Components/FeaturedDestination'
 
 const Home = () => {
   return (
     <>
       <Hero/>
+      <FeaturedDestination/>
     </>
   )
 }
