d400d75ad6e64b94f2da51aa24d648900d7d7264
Piyush1532 <piyushadav503@gmail.com>
2025-05-13 15:07:20 +0530

    added 2  componets to display

--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import Hero from '../Components/hero'
 import FeaturedDestination from '../Components/FeaturedDestination'
 import ExclusiveOffers from '../Components/ExclusiveOffers'
+import Testimonials from '../Components/Testimonials'
+import NewsLetter from '../Components/NewsLetter'
 
 const Home = () => {
   return (
@@ -9,6 +11,8 @@ const Home = () => {
       <Hero/>
       <FeaturedDestination/>
       <ExclusiveOffers/>
+      <Testimonials/>
+      <NewsLetter/>
     </>
   )
 }
