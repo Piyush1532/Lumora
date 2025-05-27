3a7d8979c1e1c5a876f8ebe3d062159280e141c6
Piyush1532 <piyushadav503@gmail.com>
2025-05-27 13:36:14 +0530

    added more routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,10 @@ import Home from './Pages/Home'
 import Footer from './Components/Footer'
 import AllRooms from './Pages/AllRooms'
 import RoomDetails from './Pages/RoomDetails'
+import MyBookings from './Pages/MyBookings'
+import HotelRegistration from './Components/HotelRegistration'
+import Layout from './Pages/hotelowner/Layout'
+import Dashboard from './Pages/hotelowner/Dashboard'
 
 
 const App = () => {
@@ -15,11 +19,16 @@ const isOwnerPath=useLocation().pathname.includes("Owner")
   return (
     <div >
       {!isOwnerPath && <Navbar/>}
+   {false &&   <HotelRegistration/>}
 <div className='min-h-[70vh]'>
 <Routes>
   <Route path='/' element={<Home/>}/>
   <Route path='/rooms' element={<AllRooms/>}/>
   <Route path='/rooms/:id' element={<RoomDetails/>}/>
+  <Route path='/my-bookings' element={<MyBookings/>}/>
+  <Route path='/owner' element={<Layout/>}>
+      <Route index element={<Dashboard/>}/>
+  </Route>
 </Routes>
 </div>
 <Footer/>
